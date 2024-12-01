71f807768c34257088cbce1b5c42974d4e4a5698
zansat-xii <zansatxii@gmail.com>
2024-12-01 11:16:21 +0100

    Update cansat code.ino

--- arduino_files/organisation code/cansat code.ino
+++ arduino_files/organisation code/cansat code.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <SoftwareSerial.h>
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -11,6 +12,9 @@
 //Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+SoftwareSerial mySerial(9,10);
+int number = 0;
 
 void setup() {
   Serial.begin(9600);
